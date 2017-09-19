75dde1097c297bf29f5358bbdb3c431bd1946335
Will Robbins <will@sighten.io>
2017-09-18 22:26:32 -0700

    add before/after to eslint globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,8 @@ module.exports = {
     globals: {
         describe: true,
         it: true,
+        before: true,
+        after: true,
         beforeEach: true,
         afterEach: true,
     },
