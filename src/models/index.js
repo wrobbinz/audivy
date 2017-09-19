478448a7db23b3de28f716fe2062b627e93dac15
Will Robbins <will@sighten.io>
2017-09-18 19:45:45 -0700

    rename db.js to index

--- src/models/index.js
+++ src/models/index.js
@@ -3,6 +3,6 @@ import mongoose from 'mongoose'
 
 mongoose.set('debug', true)
 mongoose.connect('mongodb://localhost/audivy', { useMongoClient: true })
-mongoose.Promise = global.Promise
+mongoose.Promise = Promise
 
 module.exports.User = require('./user')
